2edd763aaec59688d975d4add27681e3ba446227
didi12121 <1968967961@qq.com>
2019-11-11 22:06:08 +0800

    SpringBoot学习

--- src/main/java/com/example/didi1309/coummity/Controlle/HelloControlle.java
+++ src/main/java/com/example/didi1309/coummity/Controlle/HelloControlle.java
@@ -2,14 +2,15 @@ package com.example.didi1309.coummity.Controlle;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloControlle {
-    @RequestMapping("/hello")
-    public String hello(@RequestParam("name") String name, Model model){
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model){
         model.addAttribute("name",name);
+        System.out.println(name.toString());
         return "hello";
     }
 }
