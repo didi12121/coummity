8ef1ed8eb2729b74edbfacbcdb2ae6770bbda99b
didi12121 <1968967961@qq.com>
2019-11-11 19:11:39 +0800

    SpringBoot学习

---
+++ src/main/java/com/example/didi1309/coummity/CoummityApplication.java
@@ -0,0 +1,13 @@
+package com.example.didi1309.coummity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoummityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoummityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/didi1309/coummity/CoummityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.didi1309.coummity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoummityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
