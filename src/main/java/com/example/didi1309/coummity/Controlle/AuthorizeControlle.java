3ad020f12a7da598076dbbb6de9caf42eed5f544
didi12121 <1968967961@qq.com>
2019-11-19 00:22:52 +0800

    实现持久化登录状态

--- src/main/java/com/example/didi1309/coummity/Controlle/AuthorizeControlle.java
+++ src/main/java/com/example/didi1309/coummity/Controlle/AuthorizeControlle.java
@@ -11,8 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -36,7 +37,8 @@ public class AuthorizeControlle {
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state,
-                           HttpServletRequest request)  {
+                           HttpServletRequest request,
+                           HttpServletResponse response)  {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(secret);
@@ -47,13 +49,15 @@ public class AuthorizeControlle {
         GithubUser githubUser = gitbubProvider.getUser(accessToken);
         if (githubUser!=null){
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(githubUser.getName());
             user.setAccoundId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtModified());
             userMapper.insert(user);
-            request.getSession().setAttribute("User",githubUser);
+            //写入cookie
+            response.addCookie(new Cookie("token",token));
             return "redirect:/";
         }else {
             return "redirect:index";
--- src/main/java/com/example/didi1309/coummity/Controlle/IndexControlle.java
+++ src/main/java/com/example/didi1309/coummity/Controlle/IndexControlle.java
@@ -1,15 +1,37 @@
 package com.example.didi1309.coummity.Controlle;
 
+import com.example.didi1309.coummity.mapper.UserMapper;
+import com.example.didi1309.coummity.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @Controller
 public class IndexControlle {
+    @Autowired
+    private UserMapper userMapper;
+    //当访问首页时，通过cookie在数据库中寻找token，如果token存在，用户存在，写入session
     @GetMapping("/")
-    public String hello( Model model){
+    public String hello(HttpServletRequest request){
        // model.addAttribute("name",name);
+        Cookie[] cookies=request.getCookies();
+        if (cookies!=null){
+        for (Cookie cookie:cookies) {
+            if (cookie.getName().equals("token")){
+                String token=cookie.getValue();
+                User user = userMapper.findByToken(token);
+                if (user!=null){
+                    request.getSession().setAttribute("User",user);
+                }
+                break;
+            }
+        }}
         return "index";
     }
 }
